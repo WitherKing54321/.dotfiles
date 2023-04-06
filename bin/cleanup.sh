f8e4ca1749fd4cdb083dfbae6334c221cf223b8d
Brandon Wells <bjw010504@gmail.com>
2023-04-06 15:48:09 -0400

    Created linux shell setup scripts.

--- bin/cleanup.sh
+++ bin/cleanup.sh
@@ -2,8 +2,9 @@
 
 rm ~/.vimrc
 #Deletes the .vimrc directory from the home directory
+
 sed -i 's/[source]+/ /g' ~/.bashrc
 #Replaces the text found with nothing in the given file
 
-rm -rf ~/.TRASH/*
+rm -rf ~/.TRASH*
 #Deletes the .TRASH directory
